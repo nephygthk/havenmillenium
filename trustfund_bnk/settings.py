3307904b4c516b1d314f3efde504d426162c54b8
NephygTdkingz <tdkingzict@gmail.com>
2025-01-11 16:06:30 +0100

    added domain to host

--- trustfund_bnk/settings.py
+++ trustfund_bnk/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost','trustfundonline.top', 'www.trustfundonline.top']
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost','havenmillenium.com', 'www.havenmillenium.com']
 
 
 # Application definition
